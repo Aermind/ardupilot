3c7e685eb6d53f315942aa8ee4d98f00fb9f7d36
Aerhart <admin@aerhart.com>
2021-02-27 10:31:23 -0600

    New line test

--- ArduPlane/tiltrotor.cpp
+++ ArduPlane/tiltrotor.cpp
@@ -362,7 +362,8 @@ void QuadPlane::tiltrotor_vectored_yaw(void)
     } else {
         float yaw_out = motors->get_yaw();
         float yaw_range = zero_out;
-        // AerTilt test (no changes)
+        // AerTilt test (new line below)
+
         SRV_Channels::set_output_scaled(SRV_Channel::k_tiltMotorLeft,  1000 * (base_output + yaw_out * yaw_range));
         SRV_Channels::set_output_scaled(SRV_Channel::k_tiltMotorRight, 1000 * (base_output - yaw_out * yaw_range));
     }
