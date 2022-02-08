0f47f05a207bb1499bbdfd365cadd5cb10ce3c20
Aerhart <admin@aerhart.com>
2022-02-08 17:50:40 -0600

    test

--- libraries/AP_AHRS/AP_AHRS_View.cpp
+++ libraries/AP_AHRS/AP_AHRS_View.cpp
@@ -20,7 +20,6 @@
 
 #include "AP_AHRS_View.h"
 #include <stdio.h>
-#include "Plane.h"
 
 AP_AHRS_View::AP_AHRS_View(AP_AHRS &_ahrs, enum Rotation _rotation, float pitch_trim_deg) :
     rotation(_rotation),
@@ -28,7 +27,7 @@ AP_AHRS_View::AP_AHRS_View(AP_AHRS &_ahrs, enum Rotation _rotation, float pitch_
 {
     switch (rotation) {
     case ROTATION_NONE:
-        y_angle = SRV_Channels::get_output_norm(SRV_Channel::k_rcin8) * plane.quadplane.aparm.angle_max * 0.01f; // y_angle = 0; AerTilt
+        y_angle = 90; // 0; AerTilt
         break;
     case ROTATION_PITCH_90:
         y_angle = 90;
