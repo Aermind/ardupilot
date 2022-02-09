7ce58359b3e22482896b543afa2af547f819e88c
Aerhart <admin@aerhart.com>
2022-02-09 13:01:10 -0600

    test

--- libraries/AP_AHRS/AP_AHRS.cpp
+++ libraries/AP_AHRS/AP_AHRS.cpp
@@ -236,7 +236,7 @@ void AP_AHRS::update_orientation()
             _compass->set_board_orientation(orientation);
         }
     } else {
-        _custom_rotation.from_euler(radians(_custom_roll), radians(_custom_pitch + plane.quadplane.aparm.angle_max), radians(_custom_yaw)); // AerTilt (just a marker)
+        _custom_rotation.from_euler(radians(_custom_roll), radians(_custom_pitch), radians(_custom_yaw)); // AerTilt (just a marker)
         AP::ins().set_board_orientation(orientation, &_custom_rotation);
         if (_compass != nullptr) {
             _compass->set_board_orientation(orientation, &_custom_rotation);
@@ -374,7 +374,7 @@ void AP_AHRS::update_trig(void)
     if (_last_trim != _trim.get()) {
         float aertilt_rotation = SRV_Channels::get_output_norm(SRV_Channel::k_rcin8);
         _last_trim = _trim.get();
-        _rotation_autopilot_body_to_vehicle_body.from_euler(_last_trim.x, _last_trim.y + aertilt_rotation, 0.0f);
+        _rotation_autopilot_body_to_vehicle_body.from_euler(_last_trim.x, _last_trim.y + aertilt_rotation + plane.quadplane.aparm.angle_max, 0.0f);
         _rotation_vehicle_body_to_autopilot_body = _rotation_autopilot_body_to_vehicle_body.transposed();
     }
     // ... to here, AerTilt
