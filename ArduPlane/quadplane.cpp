a0552a3a3b897209db891d1ba11c043f52e6a433
Aerhart <admin@aerhart.com>
2022-02-18 11:18:37 -0600

    test

--- ArduPlane/quadplane.cpp
+++ ArduPlane/quadplane.cpp
@@ -2274,6 +2274,10 @@ void QuadPlane::motors_output(bool run_rate_controller)
 {
     if (run_rate_controller) {
         attitude_control->rate_controller_run();
+        if ((AP_HAL::millis() - AerLean_timer) > 1000) {
+            gcs().send_text(MAV_SEVERITY_INFO, "AerLean Option 1");
+            AerLean_timer = AP_HAL::millis();
+        }
     }
 
     /* Delay for ARMING_DELAY_MS after arming before allowing props to spin:
