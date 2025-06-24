f8de6b851e4aec0a70712fafcf98f01108f285dd
Aerhart <admin@aerhart.com>
2025-06-24 12:52:32 -0500

    test

--- ArduPlane/ArduPlane.cpp
+++ ArduPlane/ArduPlane.cpp
@@ -171,12 +171,12 @@ void Plane::ahrs_update()
     // === Aerhart Custom Servo Logic for AerFold ===
 
     float airspeed_m_s = airspeed.get_airspeed();
-    float rssi_input = hal.rcin->get_rssi() / 1.0f;
+    float rssi_input = -hal.rcin->get_rssi() / 1.0f;
 
     float C = 0.103218f * airspeed_m_s * airspeed_m_s - 1.05967f * airspeed_m_s + 1514.52f;
     float b = 0.0000702125f * airspeed_m_s * airspeed_m_s - 0.00159508f * airspeed_m_s + 0.0367758f;
 
-    float pwm_output = 2000.0f * C * b * rssi_input; // powf(rssi_input, b);
+    float pwm_output = 3000.0f * C * b * rssi_input; // powf(rssi_input, b);
     pwm_output = constrain_float(pwm_output, 1000.0f, 2000.0f);
 
     hal.rcout->write(6, (uint16_t)pwm_output);
