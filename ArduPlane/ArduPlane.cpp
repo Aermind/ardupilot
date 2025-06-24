39cd812d72a782dd350841407cae4db303fbec67
Aerhart <admin@aerhart.com>
2025-06-24 09:45:18 -0500

    test

--- ArduPlane/ArduPlane.cpp
+++ ArduPlane/ArduPlane.cpp
@@ -71,7 +71,7 @@ const AP_Scheduler::Task Plane::scheduler_tasks[] = {
     SCHED_TASK(calc_airspeed_errors,   10,    100,  42),
     SCHED_TASK(update_alt,             10,    200,  45),
     SCHED_TASK(adjust_altitude_target, 10,    200,  48),
-    SCHED_TASK(aerhart_servo_control_update, 20, 100, 155),
+    SCHED_TASK(aerhart_servo_control_update, 10, 200, 155),
 #if AP_ADVANCEDFAILSAFE_ENABLED
     SCHED_TASK(afs_fs_check,           10,    100,  51),
 #endif
@@ -259,12 +259,6 @@ void Plane::aerhart_servo_control_update()
 
     hal.rcout->write(6, (uint16_t)pwm_output);
 
-    // Throttle debug print to once per second
-    static uint32_t last_print_time = 0;
-    if (AP_HAL::millis() - last_print_time > 1000) {
-        printf("Airspeed: %.2f | RSSI: %.2f | PWM: %.2f\n", airspeed_m_s, rssi_input, pwm_output);
-        last_print_time = AP_HAL::millis();
-    }
 }
 
 
